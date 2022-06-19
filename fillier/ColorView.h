d103928e04ccac55fe558bf8546e85459e5f622e
Misha Causur <mishacausur@mail.ru>
2022-06-19 16:04:27 +0500

    ColorView interface added

---
+++ fillier/ColorView.h
@@ -0,0 +1,18 @@
+//
+//  ColorView.h
+//  fillier
+//
+//  Created by Misha Causur on 19.06.2022.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ColorView : UIView
+
+- (instancetype)initWithFrame:(CGRect)frame andColor:(UIColor *)color;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ fillier/ColorView.m
@@ -0,0 +1,20 @@
+//
+//  ColorView.m
+//  fillier
+//
+//  Created by Misha Causur on 19.06.2022.
+//
+
+#import "ColorView.h"
+
+@implementation ColorView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
