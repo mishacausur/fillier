0834142bd36fbefbb64531407ed3ad06f9352061
Misha Causur <mishacausur@mail.ru>
2022-06-19 16:35:24 +0500

    viewcontroller subviews added

--- fillier/ViewController.m
+++ fillier/ViewController.m
@@ -6,8 +6,12 @@
 //
 
 #import "ViewController.h"
+#import "ColorView.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) ColorView *blueView;
+@property (nonatomic, strong) ColorView *yellowView;
 
 @end
 
@@ -15,7 +19,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.blueView = [[ColorView alloc] initWithFrame:CGRectMake(30.0, 150.0, 100.0, 100.0) andColor:[UIColor blueColor]];
+    
+    self.yellowView = [[ColorView alloc] initWithFrame:CGRectMake(40.0, 200.0, 200.0, 200.0) andColor:[UIColor yellowColor]];
+    
+    [self.view addSubview:self.blueView];
+    [self.view addSubview:self.yellowView];
+    
+    [self.view bringSubviewToFront:self.blueView];
 }
 
 
