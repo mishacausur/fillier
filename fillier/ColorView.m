de378c6a75554f28a2d7e10e2db73dfaf0119767
Misha Causur <mishacausur@mail.ru>
2022-06-19 16:06:53 +0500

    ColorView implementation added

--- fillier/ColorView.m
+++ fillier/ColorView.m
@@ -9,12 +9,13 @@
 
 @implementation ColorView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame andColor:(UIColor *)color
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = color;
+    }
+    return self;
 }
-*/
 
 @end
