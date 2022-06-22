63bf3b07e5b6eb0846004c3aeaf4faac8bccaccd
Misha Causur <mishacausur@mail.ru>
2022-06-23 01:30:00 +0500

    car structures implementations added

--- fillier/Car.m
+++ fillier/Car.m
@@ -7,6 +7,30 @@
 
 #import "Car.h"
 
+static int count = 0;
+
 @implementation Car
+
+- (id)initWithModel:(NSString *)model andPower:(double)power
+{
+    self = [super init];
+    if (self) {
+        _model = model;
+        _power = power;
+        count += 1;
+    }
+    return self;
+}
+
+- (NSString *)info
+{
+    NSString *strInfo = [NSString stringWithFormat:@"Model: %@; power: %lf", _model, _power];
+    return strInfo;
+}
+
++ (void)printCount
+{
+    NSLog(@"Cars count = %d", count);
+}
 
 @end
--- fillier/main.m
+++ fillier/main.m
@@ -7,11 +7,21 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import "Car.h"
 
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
+        Car *car1 = [[Car alloc] initWithModel:@"BMW" andPower:330];
+        NSString *car1Info = [car1 info];
+        NSLog(@"Info about car one: %@", car1Info);
+        
+        Car *car2 = [[Car alloc] initWithModel:@"Audi" andPower:330];
+        NSString *car2Info = [car2 info];
+        NSLog(@"Info about car one: %@", car2Info);
+        
+        [Car printCount];
+        
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
